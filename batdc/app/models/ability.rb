fbe095d479029d856a00234ac4089014cb9470d3
Kevin Moore <kevinemoore@gmail.com>
2014-12-16 16:49:36 -0800

    simplify abilities.

--- batdc/app/models/ability.rb
+++ batdc/app/models/ability.rb
@@ -6,16 +6,7 @@ class Ability
     user = User.new if anonymous
 
     # All users can read all tables.
-    can :read, DbTable
+    can [:create, :read, :update, :delete], Contact
 
-    unless anonymous
-      # All registered users can create tables.
-      can :create, DbTable
-      # Users can only modify tables that belong to them.
-      can [:update, :destroy, :import_csv, :upload_csv], DbTable,
-          user_id: user.id
-      can [:new, :create, :delete, :destroy], DbRow,
-          user_id: user.id
-    end
   end
 end
