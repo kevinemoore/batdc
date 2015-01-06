16c956de843856be3f6641c3c52ffe11f140f8ae
Kevin Moore <kevinemoore@gmail.com>
2015-01-06 12:08:23 -0800

    more deployment.

--- batdc/config/application.rb
+++ batdc/config/application.rb
@@ -1,10 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
-
+#require 'rails/all'
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Batdc
   class Application < Rails::Application
